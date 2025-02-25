cea5def708ae53cca0c5ad365f773cc7c777de93
Shirtzo <shirtzo123@gmail.com>
2025-02-25 16:24:02 +0000

    added webserver

---
+++ web-service/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const counterServiceUrl = 'http://counter-service:4000/update-cpunt'; 
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+    try {
+        await axios.post(counterServiceUrl);
+    } catch (error) {
+        console.error('Error updating counter:', error);
+        res.status(500).send('Error updating counter');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://webserver:${port}`);
+});
