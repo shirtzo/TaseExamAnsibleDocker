96b57da8d4c51226e6ca09d0616d357002e46861
Shirtzo <shirtzo123@gmail.com>
2025-02-25 16:26:02 +0000

    added webserver

--- web-service/server.js
+++ web-service/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const counterServiceUrl = 'http://counter-service:4000/update-cpunt'; 
+const counterServiceUrl = 'http://counter-service:4000/increment'; 
 
 app.use(express.static('public'));
 
@@ -17,5 +17,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://webserver:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
