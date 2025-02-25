883fe9fc98dccd48641972f309745835cf08c4f3
Shirtzo <shirtzo123@gmail.com>
2025-02-25 17:25:55 +0000

    added code

--- web-service/server.js
+++ web-service/server.js
@@ -3,18 +3,23 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const counterServiceUrl = 'http://counter-service:4000/increment'; 
+const counterServiceUrl = 'http://localhost:4000/increment'; 
 
 app.use(express.static('public'));
 
-app.get('/', async (req, res) => {
+app.get('/', async (_, res) => {
     try {
-        await axios.post(counterServiceUrl);
+        const response = await axios.post('counterServiceUrl');
+        console.log("Success:", response.data);
+        res.status(200).send("Counter updated successfully");
     } catch (error) {
         console.error('Error updating counter:', error);
         res.status(500).send('Error updating counter');
     }
 });
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
